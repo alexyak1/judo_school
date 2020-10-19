74a1ef61cf791e8ea4d16e8221b902892ca2532c
alexyak1 <yakimchuk.nstu@gmail.com>
2020-10-19 20:39:47 +0200

    first commit

---
+++ quiz/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Techniques, Belt_group
+
+admin.site.register(Techniques)
+admin.site.register(Belt_group)
+# Register your models here.
---
+++ quiz/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.core.files.storage import FileSystemStorage
+
+photosPath = FileSystemStorage(location='/media/photos')
+
+class Techniques(models.Model):
+    name = models.CharField(max_length=200)
+    img_url = models.CharField(max_length=200)
+    belt_group = models.ForeignKey('Belt_group', on_delete=models.CASCADE)
+    photo = models.ImageField(null=True)
+    def __str__(self):
+        return self.name
+
+class Belt_group(models.Model):
+    belt_color = models.CharField(max_length=200)
+    def __str__(self):
+        return self.belt_color
