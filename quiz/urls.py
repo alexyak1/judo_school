8b72942e6c645c3cc04eefcaf8bd686edf300db9
alexyak1 <yakimchuk.nstu@gmail.com>
2020-10-20 21:19:54 +0200

    add view and pictures

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('<int:techniques_id>/', views.delail, name='detail'),
+    path('<int:techniques_id>/results/', views.results, name = 'results'),
+    path('<int:techniques_id>/vote/', views.vote, name='vote'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+
+from .models import Techniques
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the quiz page.")
+    latest_technic_list = Techniques.objects.order_by('-id')[:5]
+    template = loader.get_template('quiz/index.html')
+    context = {
+        'latest_technic_list': latest_technic_list,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+def delail(request, techniques_id):
+    try:
+        technic = Techniques.objects.get(pk=techniques_id)
+    except Techniques.DoesNotExist:
+        raise Http404("technic does not exist")
+    return HttpResponse("You are looking at techniq %s." % techniques_id)
+
+def results(request, techniques_id):
+    response = "You are looking at the result of question %s."
+    return HttpResponse(response % techniques_id)
+
+def vote(request, techniques_id):
+    return HttpResponse("You are voting on techniq %s." % techniques_id)
