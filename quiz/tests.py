2150569d3420a931feb2e77d36512f0b396e443e
alexyak1 <yakimchuk.nstu@gmail.com>
2020-10-29 22:56:41 +0100

    test index view

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Techniques
+from django.urls import reverse
+
+class QuizIndexViewTests(TestCase):
+    def test_no_question(self):
+        response = self.client.get(reverse('quiz:index'))
+        self.assertEqual(response.status_code, 200)
