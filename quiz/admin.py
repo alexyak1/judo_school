5dfea71b04f6a112b87de0291696c29eecb65b39
alexyak1 <yakimchuk.nstu@gmail.com>
2020-11-03 19:20:15 +0100

    configure admin page

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from .models import Techniques, Belt_group
 
-admin.site.register(Techniques)
+# admin.site.register(Techniques)
 admin.site.register(Belt_group)
 # Register your models here.
+
+
+
+class TechniquesAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['belt_group']}),
+        ('Techinc info', {'fields': ['name', 'photo']}),
+
+        ]
+
+admin.site.register(Techniques, TechniquesAdmin)
